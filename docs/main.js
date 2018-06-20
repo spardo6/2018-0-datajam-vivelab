09b733c512e9d9c31ef5eba5fd39e77aa90b94b1
Steven Pardo <dspardo6@gmail.com>
2018-06-20 17:40:46 -0500

    Mostrar marcadores en Google Maps

--- docs/main.js
+++ docs/main.js
@@ -1,31 +1,47 @@
-{
-  const data = [
-    { "name": "Vivelab Bogotá", "longitude": 4.6380851, "latitude": -74.0915043 },
-    { "name": "Universidad Nacional", "longitude": 4.6363211, "latitude": -74.0813062 },
-    { "name": "Embajada de los Estados Unidos", "longitude": 4.6345215, "latitude": -74.0908348 }
-  ];
+const data = [
+  { "name": "Vivelab Bogotá", "latitude": 4.6380851, "longitude": -74.0915043 },
+  { "name": "Universidad Nacional", "latitude": 4.6363211, "longitude": -74.0813062 },
+  { "name": "Embajada de los Estados Unidos", "latitude": 4.6366941, "longitude": -74.0948787 }
+];
 
-  const table = () => {
-    data.forEach((i) => {
-      document.querySelector('#dataset tbody').appendChild(row(i));
+class Main {
+  constructor(data) {
+    this.data = data;
+    this.center = { lat: 4.5981, lng: -74.0758 };
+    this.createMap();
+    this.table();
+  }
+
+  createMap() {
+    this.map = new google.maps.Map(document.getElementById('map'), { zoom: 11, center: this.center });
+  }
+
+  table() {
+    this.data.forEach((i) => {
+      document.querySelector('#dataset tbody').appendChild(this.row(i));
     });
   }
 
-  const row = (data) => {
+  row(data) {
+    this.marker(data.latitude, data.longitude);
     let row = document.createElement('tr');
-    row.appendChild(col(data.name));
-    row.appendChild(col(data.longitude));
-    row.appendChild(col(data.latitude));
+    row.appendChild(this.col(data.name));
+    row.appendChild(this.col(data.latitude));
+    row.appendChild(this.col(data.longitude));
     return row;
   }
 
-  const col = (text) => {
+  col(text) {
     let col = document.createElement('td');
     col.appendChild(document.createTextNode(text));
     return col;
   };
 
-  window.onload = () => {
-    table();
+  marker(lat, lng) {
+    new google.maps.Marker({ position: { lat, lng }, map: this.map });
   }
+}
+
+function initMap() {
+  new Main(data);
 }
