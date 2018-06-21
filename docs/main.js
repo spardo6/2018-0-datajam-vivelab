8bf2bcf0e7a1c20a2e2bcad0a42a53d16f9d962a
Steven Pardo <dspardo6@gmail.com>
2018-06-21 12:13:25 -0500

    Bordes mapa

--- docs/main.js
+++ docs/main.js
@@ -1,17 +1,17 @@
 class Main {
   constructor() {
     this.cols = [ 'route', 'line', 'route_sae', 'position', 'node', 'edging', 'latitude', 'longitude' ];
-    this.center = { lat: 4.5981, lng: -74.0758 };
-    this.getData().then((data) => {
-      this.data = data;
+    this.data().then((data) => {
+      this.locations = data;
       this.createMap();
       this.table();
+      this.bounds();
     }).catch(() => {
       alert('Error al cargar los datos.');
     });
   }
 
-  getData() {
+  data() {
     return new Promise((resolve, reject) => {
       let http = new XMLHttpRequest();
       http.onreadystatechange = () => {
@@ -28,18 +28,23 @@ class Main {
     });
   }
 
+  latLng(latitude, longitude) {
+    return { lat: latitude, lng: longitude };
+  }
+
+
   createMap() {
-    this.map = new google.maps.Map(document.getElementById('map'), { zoom: 11, center: this.center });
+    this.map = new google.maps.Map(document.getElementById('map'));
   }
 
   table() {
-    this.data.forEach((i) => {
+    this.locations.forEach((i) => {
       document.querySelector('#dataset tbody').appendChild(this.row(i));
     });
   }
 
   row(data) {
-    this.marker(data.latitude, data.longitude);
+    this.marker(data.latitude, data.longitude, data.edging);
     let row = document.createElement('tr');
     this.cols.forEach((i) => {
       row.appendChild(this.col(data[i]));
@@ -53,8 +58,27 @@ class Main {
     return col;
   };
 
-  marker(lat, lng) {
-    new google.maps.Marker({ position: { lat, lng }, map: this.map });
+  marker(lat, lng, title) {
+    new google.maps.Marker({ position: { lat, lng }, map: this.map, title });
+  }
+
+  bounds() {
+    let x1, x2, y1, y2;
+
+    this.locations.forEach((location) => {
+      let lat = location.latitude;
+      let lng = location.longitude;
+
+      if (!x1 || lat > x1) { x1 = lat }
+      if (!x2 || lat < x2) { x2 = lat }
+      if (!y1 || lng > y1) { y1 = lng }
+      if (!y2 || lng < y2) { y2 = lng }
+    });
+
+    let bounds = new google.maps.LatLngBounds();
+    bounds.extend(this.latLng(x1, y2));
+    bounds.extend(this.latLng(x2, y1));
+    this.map.fitBounds(bounds);
   }
 }
 
